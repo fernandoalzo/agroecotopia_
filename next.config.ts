134b33f9cfd2ebc09c81846c09ae7504de829057
fernandoalzo <34621303+fernandoalzo@users.noreply.github.com>
2026-07-19 15:44:43 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['127.0.0.1', 'localhost', '192.168.1.1'],
@@ -15,21 +14,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
       layers: true,
     };
-    if (isServer) {
-      const prismaPath = path.resolve('src/backend/db/prisma.ts');
-      config.externals.push(({ request }: { request?: string }, callback: Function) => {
-        if (request && (request === '@/backend/db/prisma' || request.includes('backend/db/prisma'))) {
-          return callback(null, `commonjs2 ${prismaPath}`);
-        }
-        callback();
-      });
-    }
     return config;
   },
 };
